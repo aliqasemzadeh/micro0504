dff6f107de0f261d4f3df7b3c238dacb9ab939ac
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-10-03 11:42:38 +0330

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    float g1,g2,g3;
+    cout << "G1 G2 G3";
+    cin >> g1 >> g2 >> g3;
+    float avg = (g1 + g2 + g3) / 3;
+    cout << "Avg:" << avg;
+    
+    return 0;
+}
